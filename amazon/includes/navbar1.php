3108ef04c1ffcbe4e8a352190f7b9234c6840631
siddarth1331 <168561488+siddarth1331@users.noreply.github.com>
2024-09-30 21:48:00 +0530

    Add files via upload

---
+++ amazon/includes/navbar1.php
@@ -0,0 +1,51 @@
+<nav class="navbar navbar-expand-lg bg-body-tertiary navbar-dark bg-dark shadow">
+    <div class="container">
+        <a class="navbar-brand" href="#">Amazon</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavDropdown">
+            <ul class="navbar-nav ms-auto">
+               
+                
+
+
+                <?php
+                if (isset($_SESSION['user'])) {
+                ?>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            <?php echo $_SESSION['user']['name']; ?>
+                        </a>
+
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="logout.php">Logout</a></li>
+                        </ul>
+                    </li>
+                <?php
+                } else {
+                ?>
+                    <li class="nav-item">
+                        <a class="nav-link active" href="register.php">Register</a>
+                    </li>
+                    
+
+                    <li class="nav-item">
+                        <a class="nav-link active" href="login.php">Login</a>
+                    </li>
+                <?php
+                }
+                ?>
+
+            </ul>
+        </div>
+    </div>
+</nav>
+<style>
+    a{
+        font-size:10px;
+        font-weight: bold;
+
+        
+    }
+</style>
